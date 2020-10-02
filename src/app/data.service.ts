27bbb2944638281ce4f38ec03993bba5d498b1f7
Munge Ryan <mungeryan@gmail.com>
2020-10-02 11:10:22 +0300

    invoking search function

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,22 +10,19 @@ export class DataService {
   gifs= new BehaviorSubject<any>([])
 
   giphyApiKey = `${environment.giphyApiKey}`;
-  gifUrl = `${environment.gifUrl}`;
   
 
   constructor(private http:HttpClient) { }
 
   getTrendingGifs(){
-    return this.http.get(`https://giphy.com/trending-gifs?
-    giphyApiKey=%${environment.giphyApiKey}&limit=30`)
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=B1uUSQIjm34RTsPssgc4b4c3PR0lWfev&limit=25&rating=g`)
     .subscribe((response: any) => {
       this.gifs.next(response.data);
     });
   }
 
   searchGifs(gifName: string){
-    return this.http.get(`https://giphy.com/search?q=${gifName}&
-    giphyApiKey=%${environment.giphyApiKey}&limit=30`)
+    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=B1uUSQIjm34RTsPssgc4b4c3PR0lWfev&q=${gifName}&limit=25&offset=0&rating=g&lang=en`,{})
     .subscribe((response: any) => {
       this.gifs.next(response.data);
     });
